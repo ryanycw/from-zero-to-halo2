2ca43de8f128b695783a91f4b1c651884f6417d7
ryanycw <ryan@ryanycw.dev>
2024-10-31 22:59:30 +0800

    chore: redirect to docs for now

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default function HomePage() {
+  redirect('/docs');
   return (
     <main className="flex h-screen flex-col justify-center text-center">
       <h1 className="mb-4 text-2xl font-bold">From Zero to Halo2</h1>
